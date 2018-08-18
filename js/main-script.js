e18ea40178cc69ab5b5aca7f3dd31f35f371d439
Dagason Hackason <dagasonhackason@gmail.com>
2018-08-18 09:45:45 +0100

    Update main-script.js

--- js/main-script.js
+++ js/main-script.js
@@ -20,20 +20,197 @@
 
        *************************************************************************************************************
        *                                                                                                           *
-       *                        Content:    JAVASCRIPT                                                             *
+       *                        Content:    CSS                                                                    *
        *                        Website:    http://www.dagasonhackason.com/                                        *
        *                                                                                                           *
        *************************************************************************************************************
 */
 
-$(document).ready(function(){
-	
-});
+body#LoginForm{
+	background-image: url("../images/long-banner.jpg"); 
+	background-repeat: no-repeat; 
+	background-position: 0px -63px; 
+	background-size: cover; 
+}
 
-function openDashboard(inputOperand) {
-	var t = window.location.toString();
-	t = t.substr(0, t.lastIndexOf("/"));
-	t = t  + "/" + inputOperand;
-	////alert(t);
-	window.location.href = t;
+body#RegisterForm { 
+	background-image: url("../images/long-banner.jpg"); 
+	background-repeat: no-repeat; 
+	background-position: 0px -63px; 
+	background-size: cover; 
+}
+
+body#LogoutPage { 
+	background-image: url("../images/long-banner.jpg"); 
+	background-repeat: no-repeat; 
+	background-position: 0px -63px; 
+	background-size: cover; 
+}
+
+body#ProfilePage { 
+	background-image: url("../images/long-banner.jpg"); 
+	background-repeat: no-repeat; 
+	background-position: 0px -63px; 
+	background-size: cover; 
+}
+
+.holder-top h2 { 
+	color: #444444; 
+	font-size: 18px; 
+	margin: 0 0 8px 0;
+}
+
+.holder-top p { 
+	color: #777777; 
+	font-size: 14px; 
+	margin-bottom: 30px; 
+	line-height:24px;
+}
+
+.login-form .form-control {
+  background: #f7f7f7 none repeat scroll 0 0;
+  border: 1px solid #d4d4d4;
+  border-radius: 4px;
+  font-size: 14px;
+  height: 50px;
+  line-height: 50px;
+  z-index: 1; 
+  padding-left: 35px;
+  position: relative; 
+}
+
+.Register-form .form-control {
+  background: #f7f7f7 none repeat scroll 0 0;
+  border: 1px solid #d4d4d4;
+  border-radius: 4px;
+  font-size: 14px;
+  height: 50px;
+  line-height: 50px;
+  z-index: 1; 
+  padding-left: 35px;
+  position: relative; 
+}
+
+.profile-div .form-control {
+  background: #f7f7f7 none repeat scroll 0 0;
+  border: 1px solid #d4d4d4;
+  border-radius: 4px;
+  font-size: 14px;
+  height: 50px;
+  line-height: 50px;
+  z-index: 1; 
+  padding-left: 35px;
+  position: relative; 
+}
+
+.main-div {
+  background: #ffffff none repeat scroll 0 0;
+  border-radius: 2px;
+  margin: 10px auto 30px;
+  min-width: 400px;
+  max-width: 45%;
+  padding: 50px 70px 70px 71px;
+  box-shadow: 0px 0px 1px #000000;
+}
+
+.login-form .form-group {
+  margin-bottom: 10px;
+}
+
+.Register-form .form-group {
+  margin-bottom: 10px;
+}
+
+.profile-div .form-group {
+  margin-bottom: 10px;
+}
+
+.login-form { 
+	text-align: center;
+}
+
+.Register-form { 
+	text-align: center;
+}
+
+.profile-div { 
+	text-align: center;
+}
+
+.forgot a {
+  color: #777777;
+  font-size: 14px;
+  text-decoration: underline;
+}
+
+.login-form .btn.btn-primary {
+  background: #333333 none repeat scroll 0 0;
+  border-color: #666666;
+  color: #ffffff;
+  font-size: 14px;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  padding: 0;
+  z-index: 1; 
+  position: relative; 
+}
+
+.Register-form  .btn.btn-primary {
+  background: #333333 none repeat scroll 0 0;
+  border-color: #666666;
+  color: #ffffff;
+  font-size: 14px;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  padding: 0;
+  z-index: 1; 
+  position: relative; 
+}
+
+.profile-div .btn.btn-danger {
+  background: #AA1000 none repeat scroll 0 0;
+  border-color: #666666;
+  color: #ffffff;
+  font-size: 14px;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  padding: 0;
+  z-index: 1; 
+  position: relative; 
+}
+
+.forgot {
+  text-align: left; margin-bottom:30px;
+}
+
+.botto-text {
+  color: #888888;
+  font-size: 14px;
+  margin: auto;
+}
+
+.login-form .btn.btn-primary.reset {
+  background: #ff9900 none repeat scroll 0 0;
+}
+
+.Register-form .btn.btn-primary.reset {
+  background: #ff9900 none repeat scroll 0 0;
+}
+
+.profile-div .btn.btn-primary.reset {
+  background: #ff9900 none repeat scroll 0 0;
+}
+
+.back { 
+	text-align: left; 
+	margin-top: 10px;
+}
+
+.back a {
+	color: #444444; 
+	font-size: 13px;
+	text-decoration: none;
 }
